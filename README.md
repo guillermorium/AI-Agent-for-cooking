49237216ab1090ac44a1df92f61b683759a24d44
Guillermo <guillermoberzosamontero@gmail.com>
2026-07-07 17:16:29 +0200

    Basic Agent

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 
-*** OBJETIVO
+## OBJETIVO
+
 Un agente de IA que aporte ideas de recetas sobre un plan semanal variado de comidas y cenas. Con él, evitarás
 la búsqueda por internet de la receta y no tendrás la obligación de dar tus datos en todas las páginas que abras
 sobre la receta en cuestión. Además, te ayudará con el cálculo de los macros.
 
 La salida del agente estará estructurada en partes:
+- Introducción
 - Ingredientes
 - Elaboración
 - Consejos o variaciones
 - Macronutrientes
 
-*** IDEAS
+## IDEAS
 - Da ideas de recetas según la estación del año (para recomendar aquellos platos que sientan mejor con el frío
 o con el calor)
 - Busca la receta en una bibliografía de documentos guardados en local (libros de recetas, recetarios o
@@ -19,6 +21,15 @@ libros de cocina)
 se están poniendo malos.
 - Calcula los macros de la receta que te está dando, y puede coordinarlos con la receta de la cena
 para cubrir algún objetivo de ganancia muscular.
+- El agente tendrá dos tipos de memoria: largo plazo (con las recetas hechas hasta el momento), a la que se añadirá la
+receta final elegida en la conversación; y corto plazo (los mensajes de la conversación mantenida), que servirá al 
+método recipe_output() para redactar la receta y exportarla (tras eso, se borrará).
+
+## ISSUES
+- Gestionar la memoria a largo plazo con el fichero json.
+- Pensar como inyectar el nombre de la receta final en el método recipe_response(), al menos la propuesta de receta.
+- Manejar la confirmación de receta con 'ok' y 'no' y los mensajes del agente. Afinar su system prompt.
+
 
 YO: Que puedo comer hoy?
 AGENTE: 
--- main.py
+++ main.py
@@ -4,22 +4,127 @@ import json
 
 class CookingAgent:
     def __init__(self):
-        # AI agent creation. It will use Claude as LLM
+        """
+        AI agent creation using ChatGPT 4.1. Loads the history of recommended ingredients and recipes (and creates the
+        json file if it doesn't exist).
+        """
+        # Agent config
         self.client = OpenAI(
             base_url="https://models.github.ai/inference",
             api_key=os.environ["GITHUB_TOKEN"]
         )
         self.model_name = "openai/gpt-4.1"
 
+        # Set agent's tools
+        self.setup_tools()
+
         # Ensure needed structure is OK
         if not os.path.exists('recipes_history.json'):
             with open("recipes_history.json", "w") as history:
                 history.write("{}")
 
+        # Reads history file (long-term memory)
+        with open('recipes_history.json', 'r') as f:
+            self.history = json.load(f)
+
+        # Short-term memory
+        self.current_session = [
+            {
+                "role": "system",
+                "content": "You're a profesional chef focused on balanced nutrition and a cooking influencer who "
+                           "likes innovation and modern recipes."
+                           ""
+                           "When you decide to write down the final recipe, you have to ask the user to confirm if "
+                           "he wants that recipe (ask him to write 'ok' or 'no')."
+            }
+        ]
 
         pass
 
     def setup_tools(self):
+        self.tools = [
+            {
+                "type": "function",
+                "function": {
+                    "name": "recipe_response",
+                    "description": "Propose a final recipe idea following the desired structure",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {},
+                        "required": []
+                    }
+                }
+            }
+        ]
+
+        pass
+
+    def normal_response(self, prompt):
+
+        recipe_idea = False
+        # Saves user's prompt
+        self.current_session.append(
+            {
+                "role": "user",
+                "content": prompt
+            }
+        )
+        # Builds a response for the user conversation
+        response = self.client.chat.completions.create(
+            model = self.model_name,
+            messages = self.current_session,
+            tools = self.tools
+        )
+        # Saves agent's response
+        self.current_session.append(response.choices[0].message)
+
+        if response.choices[0].message.tool_calls is not None:
+            for tc in response.choices[0].message.tool_calls:
+                if tc.function.name == "recipe_response":
+                    # Flag have control on the final recipe
+                    recipe_idea = True
+
+                    elaboration = self.recipe_response()
+                    self.current_session.append(
+                        {
+                            "role": "tool",
+                            "tool_call_id": tc.id,
+                            "content": elaboration
+                        }
+                    )
+                    return elaboration, recipe_idea
+
+        return response.choices[0].message.content, recipe_idea
+
+    def recipe_response(self, language="spanish"):
+        # Builds the response
+        response = self.client.chat.completions.create(
+            model = self.model_name,
+            messages = [
+                {
+                "role": "user",
+                "content": f"Explain me the recipe you are thinking about. You must structure the response into 4"
+                           f"parts:"
+                           f"- Introduction: Before listing all ingredients, you have to indicate how many people is the "
+                           f"recipe for, how long would you be cooking and the difficulty of the recipe in a 5 stars scale."
+                           f"- Ingredients:  Then, you have to list ingredients and the amount it is used."
+                           f"- Elaboration: You have to indicate different steps to follow correctly the cooking process. "
+                           f"It must be easy to understand, and you can include advises and variations (not necessary)."
+                           f"- Macros: At the end, you have to estimate, looking at the ingredients, the calories of "
+                           f"the recipe and its macros."
+                           f"- Advises: This is an optional part in which you can resume all the advises or variations "
+                           f"that the recipe could have."
+                           f"(You answer must be in {language})"
+                }
+            ]
+        )
+
+        return response.choices[0].message.content
+
+    def final_response(self):
+        # Add the recipe to the json file
+
+        # Export the response to PDF
         pass
 
     def check_history(self):
@@ -34,23 +139,17 @@ class CookingAgent:
     def macros_calculation(self):
         pass
 
-    def recipe_output(self, recipe):
-        # Builds the response
-        response = self.client.chat.completions.create(
-            model = self.model_name,
-            messages = [
-                {
-                "role": "user",
-                "content": "Dime 3 personajes de Juego de tronos"
-                }
-            ]
-        )
-
-        return response.choices[0].message.content
-
 
 if __name__ == "__main__":
     agent = CookingAgent()
+    recipe_idea = False
+    while True:
+        prompt_message = str(input("Tú: "))
 
-    response = agent.recipe_output("Cooking")
-    print(response)
+        if recipe_idea and prompt_message.lower() in ["hecho", "ok", "perfecto", "exportar"]:
+            agent.final_response()
+            break
+
+        response, recipe_idea = agent.normal_response(prompt_message)
+
+        print("Agente: ", response, recipe_idea)
